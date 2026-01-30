1e6e9f11bdd5c5d9b5cc72093202c8cbc8498139
Mike Delaney <gitlab@mldelaney.io>
2026-01-30 08:55:22 -0500

    adding logger
    Signed-off-by: Mike Delaney <gitlab@mldelaney.io>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-  println!("Hello, world!");
+use env_logger;
+use log::info;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  env_logger::init();
+  info!("Hello, world!");
+
+  Ok(())
 }
